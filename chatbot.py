4efa796de9287a54aaa951d14c4ca6c80a49afa1
Mezan Akhtar <meezakhtar@gmail.com>
2026-06-08 23:10:05 +0530

    Added project screenshots

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,11 @@
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 lemmatizer = WordNetLemmatizer()
 
 def preprocess(text):
